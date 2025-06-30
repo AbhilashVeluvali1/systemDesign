818d05081945682faf5160e63e46d68fcbffbbf7
abhilashveluvali <abhilash.veluvali@gmail.com>
2025-06-29 22:48:58 -0500

    factory Pattern 06/29

---
+++ factoryPattern/pizzaStore/NYStylePizzaStore.java
@@ -0,0 +1,12 @@
+package factoryPattern.pizzaStore;
+
+public class NYStylePizzaStore extends PizzaStore {
+
+    @Override
+    public Pizza createPizza(String type) {
+        if(type.equals("cheese")){
+            return new NYStyleCheesePizza();
+        }
+        return null;
+    }
+}
---
+++ factoryPattern/pizzaStore/PizzaStore.java
@@ -0,0 +1,16 @@
+package factoryPattern.pizzaStore;
+
+public abstract class PizzaStore {
+
+    public Pizza orderPizza(String type) {
+        Pizza pizza = createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+
+    //abstract factory method
+    public abstract Pizza createPizza(String type);
+}
